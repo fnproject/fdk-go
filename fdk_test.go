3429ad29b658fbf2bc14de8b7c2405ecdc758d1e
Reed Allman <reed@rdallman.com>
2018-09-20 16:26:04 -0700

    WIP: first rev of fdk 2.0
    would like some eyes before going whole hog please :)
    
    largely, this changes the context object to an interface, which makes doing
    shenanigans with the headers nice and transparent to users, where they'll
    access the headers the same whether they are http gateway headers or invoke
    headers. this also drops support for all non-http-stream formats, be gone
    pests. this also gets rid of the utils package and makes this all top level
    again instead of coercing contexts back and forth, since we don't need to futz
    with pipes for tests maybe we can get creative without having a separate
    package for that stuff anymore.
    
    useful points of feedback I'm asking for without making any presuppositions by
    committing something (but don't let that starve your critical creativity):
    
    do you want `hctx := fdk.GetHTTPContext(ctx)` ? should it return nil if it's
    not an http context? should we make the user cast so that they have to panic
    and not us? do lions have the ability to communicate with dolphins? one thing
    to consider is that there is overlap between the http context and vanilla
    context, so using the vanilla context type from `fdk.GetContext(ctx)` will
    still allow getting at the (modified) headers and config, and getting at the
    http context is only required for the request method and url (for users).
    
    this totally breaks the API since now these things are addressed by interface
    methods instead of fields on a concrete type. I should have been smacked 12
    months ago, I blame everyone else no way this was my fault, nope. is this OK?
    should we worry about compat? the changes are really easy in a lot of cases
    (add parens...) and the trade offs seem ok (and we don't have a stable API and
    the heat death of the universe is imminent?)
    
    i really did try to make the godoc look nice. if you are anywhere near as anal
    as i am i would appreciate a review of `$ godoc .`
    
    i'm hesitant to make the out writer on the handler an http.ResponseWriter, but
    it would make a whole lot of sense. kind of the whole point was to get away
    from that, but in any event if we do want this, it's a good chance to do it.
    we can change the handler signature if we'd like but that may be a slightly
    harder breakage for users to fix up. open to ideas anyway.
    
    ... if this is the right path forward, I will add more tests to this and go
    fix up the test image in fn (possibly punting the latter for a couple weeks
    off when i'll have time to properly kill format in fn).

--- fdk_test.go
+++ fdk_test.go
@@ -1,22 +1,19 @@
 package fdk
 
 import (
-	"bufio"
-	"bytes"
 	"context"
-	"encoding/json"
-	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
-	"net/http/httputil"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
-
-	"github.com/fnproject/fdk-go/utils"
 	"time"
 )
+
+// XXX(reed): test cloudevents in http-stream land
 
 func echoHTTPHandler(_ context.Context, in io.Reader, out io.Writer) {
 	io.Copy(out, in)
@@ -24,187 +21,53 @@ func echoHTTPHandler(_ context.Context, in io.Reader, out io.Writer) {
 	SetHeader(out, "yo", "dawg")
 }
 
-func TestHandler(t *testing.T) {
-	inString := "yodawg"
-	var in bytes.Buffer
-	io.WriteString(&in, inString)
+func TestHTTPStreamSock(t *testing.T) {
+	// XXX(reed): move to fdk_linux_test.go with build tag
+	// XXX(reed): extract the underlying server handler / write tests against it instead of starting uds for other tests
 
-	var out bytes.Buffer
-	echoHTTPHandler(utils.BuildCtx(), &in, &out)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	if out.String() != inString {
-		t.Fatalf("this was supposed to be easy. strings no matchy: %s got: %s", inString, out.String())
-	}
-}
-
-func TestDefault(t *testing.T) {
-	inString := "yodawg"
-	var in bytes.Buffer
-	io.WriteString(&in, inString)
-
-	var out bytes.Buffer
-
-	utils.DoDefault(HandlerFunc(echoHTTPHandler), utils.BuildCtx(), &in, &out)
-
-	if out.String() != inString {
-		t.Fatalf("strings no matchy: %s got: %s", inString, out.String())
-	}
-}
-
-func JSONHandler(_ context.Context, in io.Reader, out io.Writer) {
-	var person struct {
-		Name string `json:"name"`
-	}
-	json.NewDecoder(in).Decode(&person)
-	if person.Name == "" {
-		person.Name = "world"
-	}
-
-	body := fmt.Sprintf("Hello %s!\n", person.Name)
-	SetHeader(out, "Content-Type", "application/json")
-	err := json.NewEncoder(out).Encode(body)
+	tmpSock, err := ioutil.TempDir("/tmp", "fdk-go-test")
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
+		t.Fatal("couldn't make tmpdir for testing")
 	}
-}
+	defer os.RemoveAll(tmpSock)
 
-func JSONWithStatusCode(_ context.Context, _ io.Reader, out io.Writer) {
-	SetHeader(out, "Content-Type", "application/json")
-	WriteStatus(out, 201)
-}
+	tmpSock = filepath.Join(tmpSock, "fn.sock")
 
-func TestJSON(t *testing.T) {
-	req := &utils.JsonIn{
-		CallID:      "someid",
-		Body:        `{"name":"john"}`,
-		ContentType: "application/json",
-		Deadline:    "2018-01-30T16:52:39.786Z",
-		Protocol: utils.CallRequestHTTP{
-			Type:       "http",
-			RequestURL: "http://localhost:8080/r/myapp/yodawg",
-			Headers:    http.Header{},
-			Method:     "POST",
+	go startHTTPServer(ctx, HandlerFunc(echoHTTPHandler), "unix:"+tmpSock)
+
+	// let the uds server start... could inotify but don't want the dependency for tests...
+	time.Sleep(1 * time.Second)
+
+	client := http.Client{
+		Transport: &http.Transport{
+			MaxIdleConns:        1,
+			MaxIdleConnsPerHost: 1,
+			IdleConnTimeout:     1 * time.Second,
+			DialContext: func(ctx context.Context, _, _ string) (net.Conn, error) {
+				var d net.Dialer
+				return d.DialContext(ctx, "unix", tmpSock)
+			},
 		},
 	}
 
-	var in bytes.Buffer
-	err := json.NewEncoder(&in).Encode(req)
-	if err != nil {
-		t.Fatal("Unable to marshal request")
-	}
-
-	var out, buf bytes.Buffer
-
-	err = utils.DoJSONOnce(HandlerFunc(JSONHandler), utils.BuildCtx(), &in, &out, &buf, make(http.Header))
-	if err != nil {
-		t.Fatal("should not return error", err)
-	}
-
-	JSONOut := &utils.JsonOut{}
-	err = json.NewDecoder(&out).Decode(JSONOut)
-
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if !strings.Contains(JSONOut.Body, "Hello john!") {
-		t.Fatalf("Output assertion mismatch. Expected: `Hello john!\n`. Actual: %v", JSONOut.Body)
-	}
-	if JSONOut.Protocol.StatusCode != 200 {
-		t.Fatalf("Response code must equal to 200, but have: %v", JSONOut.Protocol.StatusCode)
-	}
-}
-
-func TestFailedJSON(t *testing.T) {
-	dummyBody := "should fail with this"
-	in := strings.NewReader(dummyBody)
-
-	var out, buf bytes.Buffer
-
-	JSONOut := &utils.JsonOut{}
-	err := utils.DoJSONOnce(HandlerFunc(JSONHandler), utils.BuildCtx(), in, &out, &buf, make(http.Header))
-	if err != nil {
-		t.Fatal("should not return error", err)
-	}
-
-	err = json.NewDecoder(&out).Decode(JSONOut)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	if JSONOut.Protocol.StatusCode != 500 {
-		t.Fatalf("Response code must equal to 500, but have: %v", JSONOut.Protocol.StatusCode)
-	}
-}
-
-func TestJSONEOF(t *testing.T) {
-	var in, out, buf bytes.Buffer
-
-	err := utils.DoJSONOnce(HandlerFunc(JSONHandler), utils.BuildCtx(), &in, &out, &buf, make(http.Header))
-	if err != io.EOF {
-		t.Fatal("should return EOF")
-	}
-}
-
-func TestJSONOverwriteStatusCodeAndHeaders(t *testing.T) {
-	var out, buf bytes.Buffer
-	req := &utils.JsonIn{
-		CallID:      "someid",
-		Body:        `{"name":"john"}`,
-		ContentType: "application/json",
-		Deadline:    "2018-01-30T16:52:39.786Z",
-		Protocol: utils.CallRequestHTTP{
-			Type:       "json",
-			RequestURL: "http://localhost:8080/r/myapp/yodawg",
-			Headers:    http.Header{},
-			Method:     "POST",
-		},
-	}
-
-	var in bytes.Buffer
-	err := json.NewEncoder(&in).Encode(req)
-	if err != nil {
-		t.Fatal("Unable to marshal request")
-	}
-
-	err = utils.DoJSONOnce(HandlerFunc(JSONWithStatusCode), utils.BuildCtx(), &in, &out, &buf, make(http.Header))
-	if err != nil {
-		t.Fatal("should not return error", err)
-	}
-
-	JSONOut := &utils.JsonOut{}
-	err = json.NewDecoder(&out).Decode(JSONOut)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	if JSONOut.Protocol.StatusCode != 201 {
-		t.Fatalf("Response code must equal to 201, but have: %v", JSONOut.Protocol.StatusCode)
-	}
-	cType := JSONOut.Protocol.Headers.Get("Content-Type")
-	if !strings.Contains(cType, "application/json") {
-		t.Fatalf("Response content type should be application/json in this test, but have: %v", cType)
-	}
-}
-
-func TestHTTP(t *testing.T) {
-	// simulate fn writing us http requests...
-
+	// TODO headers?
 	bodyString := "yodawg"
-	in := HTTPreq(t, bodyString)
-
-	var out bytes.Buffer
-	ctx := utils.BuildCtx()
-	err := utils.DoHTTPOnce(HandlerFunc(echoHTTPHandler), ctx, in, &out, &bytes.Buffer{}, make(http.Header))
+	req, err := http.NewRequest("POST", "http://localhost/call", strings.NewReader(bodyString))
 	if err != nil {
-		t.Fatal("should not return error", err)
+		t.Fatal("error making req", err)
 	}
 
-	res, err := http.ReadResponse(bufio.NewReader(&out), nil)
+	res, err := client.Do(req)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("error doing uds request", err)
 	}
+	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusTeapot+2 {
-		t.Fatal("got wrong status code", res.StatusCode)
+		t.Fatal("got wrong status code:", res.StatusCode)
 	}
 
 	outBody, err := ioutil.ReadAll(res.Body)
@@ -213,110 +76,10 @@ func TestHTTP(t *testing.T) {
 	}
 
 	if res.Header.Get("yo") != "dawg" {
-		t.Fatal("expected yo dawg header, didn't get it")
+		t.Fatal("expected yo dawg header, didn't get it", res.Header)
 	}
 
 	if string(outBody) != bodyString {
-		t.Fatal("strings no matchy for http", string(outBody), bodyString)
+		t.Fatal("body mismatch:", string(outBody), bodyString)
 	}
-
-}
-
-func TestHTTPEOF(t *testing.T) {
-	var in bytes.Buffer
-	var out bytes.Buffer
-	ctx := utils.BuildCtx()
-
-	err := utils.DoHTTPOnce(HandlerFunc(echoHTTPHandler), ctx, &in, &out, &bytes.Buffer{}, make(http.Header))
-	if err != io.EOF {
-		t.Fatal("should return EOF")
-	}
-}
-
-func HTTPreq(t *testing.T, bod string) io.Reader {
-	req, err := http.NewRequest("GET", "http://localhost:8080/r/myapp/yodawg", strings.NewReader(bod))
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	byts, err := httputil.DumpRequestOut(req, true)
-	if err != nil {
-		t.Fatal(err)
-	}
-	return bytes.NewReader(byts)
-}
-
-func setupTestFromRequest(t *testing.T, data interface{}, contentType, nameTest string) {
-	req := &utils.CloudEventIn{
-		CloudEvent: utils.CloudEvent{
-			EventID:          "someid",
-			Source:           "fn-api",
-			EventType:        "test-type",
-			EventTypeVersion: "1.0",
-			EventTime:        time.Now(),
-			ContentType:      contentType,
-			Data:             data,
-		},
-		Extensions: utils.CloudEventInExtension{
-			Deadline: "2018-01-30T16:52:39.786Z",
-			Protocol: utils.CallRequestHTTP{
-				Type:       "http",
-				RequestURL: "http://localhost:8080/r/myapp/yodawg",
-				Headers:    http.Header{},
-				Method:     "POST",
-			},
-		},
-	}
-	var in bytes.Buffer
-	err := json.NewEncoder(&in).Encode(req)
-	if err != nil {
-		t.Fatal("Unable to marshal request")
-	}
-	t.Log(in.String())
-	var out, buf bytes.Buffer
-
-	err = utils.DoCloudEventOnce(HandlerFunc(JSONHandler), utils.BuildCtx(),
-		&in, &out, &buf, make(http.Header))
-	if err != nil {
-		t.Fatal("should not return error", err)
-	}
-
-	t.Log(out.String())
-	ceOut := &utils.CloudEventOut{}
-	err = json.NewDecoder(&out).Decode(ceOut)
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	if ceOut.Extensions.Protocol.StatusCode != 200 {
-		t.Fatalf("Response code must equal to 200, but have: %v", ceOut.Extensions.Protocol.StatusCode)
-	}
-
-	var respData string
-	json.Unmarshal([]byte(ceOut.Data.(string)), &respData)
-
-	if respData != "Hello "+nameTest+"!\n" {
-		t.Fatalf("Output assertion mismatch. Expected: `Hello %v!\n`. Actual: %v", nameTest, ceOut.Data)
-	}
-}
-
-func TestCloudEventWithJSONData(t *testing.T) {
-	data := map[string]string{
-		"name": "John",
-	}
-	contentType := "application/json"
-	setupTestFromRequest(t, data, contentType, "John")
-}
-
-func TestCloudEventWithStringData(t *testing.T) {
-	data := `{"name":"John"}`
-	contentType := "text/plain"
-	setupTestFromRequest(t, data, contentType, "John")
-}
-
-func TestCloudEventWithPerfectlyValidJSONValue(t *testing.T) {
-	// https://tools.ietf.org/html/rfc7159#section-3
-	data := false
-	contentType := "application/json"
-	setupTestFromRequest(t, data, contentType, "world")
 }
