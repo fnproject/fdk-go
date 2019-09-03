5ed486d84ddf677d5cceb838a336704057eef3c0
Anton Eicher <anton.eicher@oracle.com>
2019-09-03 13:45:07 -0700

    Harden mod shell script

--- images/init/boilerplate/mod.sh
+++ images/init/boilerplate/mod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
-go mod init "$FN_FUNCTION_NAME"
-go get
+set -euo pipefail
+go mod init "$FN_FUNCTION_NAME" > /dev/null 2>&1
+go get > /dev/null 2>&1
 tar c go.mod func.go func.init.yaml
