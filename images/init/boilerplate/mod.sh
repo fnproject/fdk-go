6e09908b1ff6354355934e37158817f62cae798d
Anton Eicher <anton.eicher@oracle.com>
2019-09-28 21:48:35 +0200

    Preserve stderr output from init image commands, as it helps with debugging. Fn CLI only untars stdout.

--- images/init/boilerplate/mod.sh
+++ images/init/boilerplate/mod.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -euo pipefail
-go mod init "$FN_FUNCTION_NAME" > /tmp/go_mod_output 2>&1
-go get > /tmp/go_get_output 2>&1
+
+# Pipe stdout to file, as Fn CLI init-image reads stdout to untar (outputting messages to stderr is fine).
+go mod init "$FN_FUNCTION_NAME" > /tmp/go_mod_output
+go get > /tmp/go_get_output
+
 tar c go.mod func.go func.init.yaml
