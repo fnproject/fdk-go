d85c272668f33f1bcfc5ced9b264c9b5b8725d26
Gaurav Saini <gaurav.s.saini@oracle.com>
2021-12-13 11:47:20 +0530

    Started using OL8 as base

--- internal/cache_go_images.sh
+++ internal/cache_go_images.sh
@@ -6,5 +6,4 @@ set -ex
 # Hence, one may encounter rate limiting issue while accessing the python docker images which are not present in artifactory.
 # In order to resolve the rate limiting issue, the below script helps to pull the python images from docker hub and cache them in artifactory.
 
-docker pull docker-remote.artifactory.oci.oraclecorp.com/golang:1.15-alpine3.13
-docker pull docker-remote.artifactory.oci.oraclecorp.com/alpine:3.13
+docker pull docker-remote.artifactory.oci.oraclecorp.com/oraclelinux:8-slim
