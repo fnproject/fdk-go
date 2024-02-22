9aa6a099582a732ea6c417cfff175c03d9a45f8e
CI <ci@fnproject.com>
2024-02-22 05:43:38 +0000

    Releasing version v0.0.41

--- version.go
+++ version.go
@@ -23,7 +23,7 @@ import (
 )
 
 // Version is the FDK version
-const Version = "0.0.40"
+const Version = "0.0.41"
 
 var versionHeader = fmt.Sprintf("fdk-go/%s", Version)
 var runtimeHeader = fmt.Sprintf("go/%s", strings.TrimLeft(runtime.Version(), "go"))
