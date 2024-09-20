59a379b9d1254cc9470960a910e422186809928f
Alex Shand <alex.shand@oracle.com>
2024-09-20 14:43:34 +0100

    Add publish to github script and remove files only used by Teamcity

--- internal/cache_go_images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# Artifactory functions as a caching proxy for DockerHub. Hence any image pulled from dockerhub will be cached in artifactory.
-# The cached images will be removed as part of cleanup if not downloaded again within a particular time frame, currently set to 6 days.
-# Hence, one may encounter rate limiting issue while accessing the python docker images which are not present in artifactory.
-# In order to resolve the rate limiting issue, the below script helps to pull the python images from docker hub and cache them in artifactory.
-
-docker pull container-registry.oracle.com/os/oraclelinux:8-slim
