e9837ba593adf75d2150e12038339d7c98a45cab
phill.denness <phill.denness@oracle.com>
2024-10-21 18:39:47 +0100

    Automate docker push and remove workaround code

---
+++ internal/release/docker_publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+REGCTL_BIN=regctl
+# Test regctl is on path
+$REGCTL_BIN --help
+
+TEMPDIR=$(mktemp -d)
+cd "${TEMPDIR}"
+
+function cleanup {
+    rm -rf "${TEMPDIR}"
+}
+trap cleanup EXIT
+
+{
+  $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/go:1.20 docker.io/fnproject/go:1.20;
+  $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/go:1.20-dev docker.io/fnproject/go:1.20-dev;
+  $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/go:1.23 docker.io/fnproject/go:1.23;
+  $REGCTL_BIN image copy iad.ocir.io/oraclefunctionsdevelopm/fnproject/go:1.23-dev docker.io/fnproject/go:1.23-dev;
+}
